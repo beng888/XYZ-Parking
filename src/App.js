cd57d45ec7a7416f3beaabc9bef168f2832c9c3b
ljsardosa <ljsardosa@multisyscorp.com>
2021-10-27 03:31:47 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import EditBar from "components/EditBar";
+import Notification from "components/Notification";
+import ManageBar from "components/ManageBar";
+import StatusBar from "components/StatusBar";
+import ParkingLot from "./components/ParkingLot";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-white">
+      <Notification />
+      <StatusBar />
+      <EditBar />
+      <ParkingLot />
+      <ManageBar />
     </div>
   );
 }
